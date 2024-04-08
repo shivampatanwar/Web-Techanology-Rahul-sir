83573c104568258bac14da9a5cc7f6f4ee85dc20
Shivam Patanwar <shivampatanwarofficial@gmail.com>
2024-04-08 08:35:39 +0530

    s

---
+++ JavaScript/Operator/operator.js
@@ -0,0 +1,15 @@
+var a = 10;
+        var b = "10";
+        var c = true;
+        var d = undefined;
+        var e = null;
+        var f = 10000*50000;
+        var g = NaN;
+
+        console.log(" Type of a : " + typeof a );
+        console.log(" Type of b : " + typeof b );
+        console.log(" Type of c : " + typeof c );
+        console.log(" Type of d : " + typeof d );
+        console.log(" Type of e : " + typeof e );
+        console.log(" Type of f : " + typeof f );
+        console.log(" Type of g : " + typeof g );
