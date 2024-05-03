16b9c251c91e126d16d5a801e9b82436a6a70f16
Shivam Patanwar <shivampatanwarofficial@gmail.com>
2024-05-03 10:55:27 +0530

    s

--- JavaScript/DOM/Task/Calculator/index.js
+++ JavaScript/DOM/Task/Calculator/index.js
@@ -1,16 +1,32 @@
 let ip = document.querySelector('input');
 
+const audio = new Audio("https://www.myinstants.com/en/instant/vine-boom-sound-70972/?utm_source=copy&utm_medium=share");
+
+
+
 function equal() {
     let ipvalue = ip.value;
     ip.value = eval(ipvalue);
+    audio.play();
 }
 
 function ac() {
     ip.value = '';
+    audio.play();
 }
 
 function number(a) {
     let ipvalue = ip.value;
     ip.value =  ipvalue+a;
+
+    audio.play();
 }
+
+
+ip.addEventListener("keypress", function(event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+      equal();
+    }
+  });
 
